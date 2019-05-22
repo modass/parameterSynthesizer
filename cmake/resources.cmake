a6c3c4e01e4d5d7dfbde3ef175b327a88d17f63f
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2019-05-22 23:16:28 +0200

    remove boost version restriction.

--- cmake/resources.cmake
+++ cmake/resources.cmake
@@ -3,7 +3,7 @@
 set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
-find_package(Boost 1.65.0 COMPONENTS program_options)
+find_package(Boost COMPONENTS program_options)
 
 if(Boost_FOUND)
     include_directories(${Boost_INCLUDE_DIRS})
